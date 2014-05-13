5ebf49885df5e0e2aebe507a13abb71c385ff743
Eric Dahl <eric.c.dahl@gmail.com>
2014-05-12 20:09:26 -0500

    add basic url parsing. fixes #1

--- src/github.com/ericdahl/go-mersenne-primes/server.go
+++ src/github.com/ericdahl/go-mersenne-primes/server.go
@@ -5,6 +5,8 @@ import (
     "net/http"
     "html"
     "log"
+    "strings"
+    "strconv"
 )
 
 func main() {
@@ -14,8 +16,11 @@ func main() {
 
 func handleRequest(w http.ResponseWriter, r *http.Request) {
     log.Printf("Processing [%q]", html.EscapeString(r.URL.Path))
-    
-    result := isPrime(7)    
-    
-    fmt.Fprintf(w, "%t", result);
+
+    p, e := strconv.Atoi(strings.Trim(r.URL.Path, "/"))
+    if e != nil {
+        fmt.Println(e)
+    }
+
+    fmt.Fprintf(w, "%t", isPrime(p));
 }
